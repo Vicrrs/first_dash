6ca0e85ce9ba436288dee6827b2192ba2758a2e0
Vicrrs <enricofermi2019@gmail.com>
2023-03-13 13:39:38 -0400

    layout dash

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import dash
+from dash import html
+import dash_core_components as dcc
 import plotly.express as px
 import pandas as pd
 
@@ -10,3 +12,19 @@ df = pd.DataFrame({
     "Amount": [4, 1, 2, 2, 4, 5],
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City")
+
+# Trabalhando com layout
+app.layout = html.Div(id="div1",
+                      children=[
+                          html.H1("First Dash", id="div1"),
+
+                          html.Div("Dash: Um framework web para Python"),
+
+                          dcc.Graph(figure=fig, id="graph")
+                      ]
+                      )
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
